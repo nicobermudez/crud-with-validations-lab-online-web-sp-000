a07f7538094cf22f9f45eaf3a6d7a52f1e2e6a9f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-18 17:10:20 +0000

    Automatically backed up by Learn

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,2 @@
 Song.create(title: "Estamos Bien", released: true, release_year: "2019", artist_name: "Bad Bunny", genre: "Reggaeton")
+Song.create(title: "Meridian", released: true, release_year: "2018", artist_name: "Odesza", genre: "Electronic")
